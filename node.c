3b9520d5d3526d29389a297a59a94ca0ffdf18e3
Katherine <ksilber1@umbc.edu>
2017-04-12 20:17:27 -0400

    c file extension

---
+++ node.c
@@ -0,0 +1,78 @@
+#include "node.h"
+#include "board.h"
+
+
+void initializeRoot(node* root, struct board *boardState){
+
+    // Setting default values
+    root->parent = NULL;
+    root->childMoves = (int *)malloc(2 * sizeof(int));
+    root->numChildren = 0;
+    root->numWins = 0;
+    root->board = *boardState;
+    root->numSimulations = 0;
+    root->children = (node **)malloc(sizeof(node*));
+}
+
+
+node* expand(node* parentNode, int* action, int name){
+
+    // Dynamically allocate new struct node
+    node* newNode = (node *)malloc(sizeof(node));
+
+    // Setting member variables for new node
+    newNode->parent = parentNode;
+    newNode->childMoves = (int *)malloc(2 * sizeof(int));
+    newNode->numChildren = 0;
+    newNode->numWins = 0;
+    newNode->numSimulations = 0;
+    newNode->children = (node **)malloc(sizeof(node*));
+    //initBoard(&newNode->currBoard);
+    //copyBoard(&parentNode->board, &newNode->board);
+
+    // Update parent node
+    parentNode->children = (node **)realloc(parentNode->children, (parentNode->numChildren + 1) * sizeof(node*));
+    parentNode->children[parentNode->numChildren] = newNode;
+    parentNode->childMoves = (int *)realloc(parentNode->childMoves, (parentNode->numChildren + 1) * 2 * sizeof(int));
+    parentNode->childMoves[parentNode->numChildren * 2] = action[0];
+    parentNode->childMoves[parentNode->numChildren * 2 + 1] = action[1];
+    parentNode->numChildren += 1;
+
+    return newNode;
+}
+
+// Bootstrap function for recursion
+void deconstructTree(node* rootNode){
+    deleteNodes(rootNode);
+    return;
+}
+
+void deleteNodes(node* currNode){
+
+    node* currParent = NULL;
+
+    // Go through children to the bottom of the right side of the tree
+    if(currNode->numChildren != 0){
+        deleteNodes(currNode->children[currNode->numChildren - 1]);
+	return;
+    }
+
+    // Update parent
+    if (currNode->parent != NULL){
+        currNode->parent->numChildren -= 1;
+        currNode->parent->children[currNode->parent->numChildren] = NULL;
+        currParent = currNode->parent;
+    }
+
+    // Delete
+    free(currNode->children);
+    free(currNode->childMoves);
+    free(currNode);
+    currNode = NULL;
+
+    if (currParent != NULL){
+	deleteNodes(currParent);
+    }
+
+}
+
--- node.h
+++ node.h
@@ -1,29 +1,39 @@
-/*
- * This file contains the method definitions for MCTS node code.
- */
-
 #ifndef NODE_H
 #define NODE_H
 
-#include "board.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "board.h"
 
-typedef struct Node
-{
-	Node* parent;
-	struct board board;
-	Node** children;
-	int* childMoves;
-	int numChildren;
-	int numWins;
-	int numSimulations;
-} node;
+typedef struct node node;
+
+typedef struct node{
+	node* parent;		// Pointer to parent node
+	struct board board;	// State of board at this node
+	int* childMoves;	// How to reach child nodes
+	int numChildren;	// Num children nodes 
+	int numWins;		// Num wins from this node
+	int numSimulations;	// Num simulations from this node
+	node** children;		// Pointers to children
+}node;
+
+
+// Use to create first parent node
+ //const node DEFAULT_VALUES = {NULL, {}, {}, 0, 0, 0};
 
 void initializeRoot(node* root, struct board *boardState);
-node* expand(node *parentNode, int *action);
+
+/* Dynamically allocate a new struct node.
+ * Attach it to the provided parent node.
+ * Do action on board.
+ * Return a pointer to the new node.
+ * Uses copyBoard() to copy previous board's data into the new node.
+ */
+node* expand(node* parentNode, int* action, int id);
+
+
+/* Recursively free all of the tree nodes starting at the provided root */
 void deconstructTree(node* rootNode);
 void deleteNodes(node* currNode);
 
 #endif
-
